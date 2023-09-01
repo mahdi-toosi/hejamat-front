87131cffdfd0b279d874fce337a20d6754795fad
mahdi toosi <pugnacious0571@gmail.com>
2023-09-01 14:44:50 +0330

    feat: add logo to header

--- src/components/TheHeader.svelte
+++ src/components/TheHeader.svelte
@@ -1,8 +1,11 @@
+<script>
+	import logo from 'assets/icons/hejam-at-logo.png'
+</script>
+
 <header>
 	<input type="checkbox" id="check" />
 	<label for="check" class="icons">
 		<svg id="menu-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-			<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 			<path
 				fill="white"
 				d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
@@ -10,7 +13,6 @@
 		</svg>
 
 		<svg id="close-icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
-			<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 			<path
 				fill="white"
 				d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
@@ -26,11 +28,7 @@
 	<nav />
 
 	<a href="#">
-		<img
-			src="https://github.com/ecemgo/today-i-learned-app/assets/13468728/4f2923ea-9eca-4221-8b18-7198f78fd04e"
-			alt="logo"
-			class="logo"
-		/>
+		<img src={logo} alt="logo" class="logo" />
 	</a>
 </header>
 
@@ -42,7 +40,7 @@
 	}
 
 	.logo {
-		width: 40px;
+		height: 40px;
 	}
 
 	.nav-item {
